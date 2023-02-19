f67f3b2863b9ea6295a0bbfbe778de3a3186cd17
Daniel Hernandez <daniel66670@hotmail.com>
2023-02-19 10:05:08 -0600

    Procesamiento de datos faltantes en el dataset

--- datasets/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing_template.py
+++ datasets/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing_template.py
@@ -16,7 +16,15 @@ import pandas as pd
 # Importar el data set
 dataset = pd.read_csv('Data.csv')
 X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, 3].values
+y = dataset.iloc[:, -1].values       
+
+# Tratamiento de los NAs
+from sklearn.impute import SimpleImputer
+imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+imputer = imputer.fit(X[:,1:3])
+X[:, 1:3] = imputer.transform(X[:, 1:3])
+
+
 
 
 # Dividir el data set en conjunto de entrenamiento y conjunto de testing
