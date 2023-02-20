c5c1c67434d52f6e0dfb1c034066c9e836d6dbd0
Daniel Hernandez <daniel66670@hotmail.com>
2023-02-19 21:45:01 -0600

    Procesado de variables categóricas

--- datasets/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing_template.py
+++ datasets/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing_template.py
@@ -24,13 +24,31 @@ imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
 imputer = imputer.fit(X[:,1:3])
 X[:, 1:3] = imputer.transform(X[:, 1:3])
 
+# Codificar datos categóricos
+# Para cambiar los labels por números de 1 a N
+#from sklearn import preprocessing
+#le_X = preprocessing.LabelEncoder()
+#X[:,0] = le_X.fit_transform(X[:,0])
 
+# Convertir a Variable dummy
+# Utilizando un vector donde cada posición representa un dato categórico y cada vector solo contiene 0s y un 1
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
+ 
+ct = ColumnTransformer(
+    [('one_hot_encoder', OneHotEncoder(categories='auto'), [0])],   
+    remainder='passthrough'                        
+)
+X = np.array(ct.fit_transform(X), dtype=float)
+
+from sklearn import preprocessing
+le_y = preprocessing.LabelEncoder()
+y = le_y.fit_transform(y)
 
 
 # Dividir el data set en conjunto de entrenamiento y conjunto de testing
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
-
 
 # Escalado de variables
 from sklearn.preprocessing import StandardScaler
